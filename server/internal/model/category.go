2dd2b49f493cbd1cea1c7fecc8a59378030c49ae
Bernardo <bernardobonillac@protonmail.com>
2026-06-02 15:00:59 -0600

    fix: add JSON tags to category model structs so mutation endpoints return lowercase keys

--- server/internal/model/category.go
+++ server/internal/model/category.go
@@ -1,19 +1,19 @@
 package model
 
 type Category struct {
-	ID        string
-	GroupID   string
-	Name      string
-	Hidden    bool
-	SortOrder int
+	ID        string     `json:"id"`
+	GroupID   string     `json:"group_id"`
+	Name      string     `json:"name"`
+	Hidden    bool       `json:"hidden"`
+	SortOrder int        `json:"sort_order"`
 }
 
 type CategoryGroup struct {
-	ID         string
-	Name       string
-	SortOrder  int
-	Hidden     bool
-	Categories []Category
+	ID         string     `json:"id"`
+	Name       string     `json:"name"`
+	SortOrder  int        `json:"sort_order"`
+	Hidden     bool       `json:"hidden"`
+	Categories []Category `json:"categories"`
 }
 
 type CreateCategoryReq struct {
